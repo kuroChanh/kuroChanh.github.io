0de5b6cafceedc36a80a7cd6cfbe918e870112d1
kuroChanh <tuyetnhi@hotmail.ca>
2018-05-07 22:55:27 -0400

    update

---
+++ js/gameGallery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	//the gallery for the four game columns
+	var ps4Col = document.getElementById('ps4Gallery');
+	var dsCol = document.getElementById('dsGallery');
+	var mobCol = document.getElementById('mobileGallery');
+	var pcCol = document.getElementById('laptopGallery');
+	//image arrays
+	var ps4Gallery;
+	var dsGallery;
+	var mobGallery;
+	var pcGallery;
+});
