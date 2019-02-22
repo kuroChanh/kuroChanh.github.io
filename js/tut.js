768665e35d02d9fbc641b9d4246f2b7bb9df95a6
kuroChanh <tuyetnhi@hotmail.ca>
2019-02-21 18:41:12 -0800

    brickbreaker - finished setting up the game from the tutorial

--- js/tut.js
+++ js/tut.js
@@ -15,6 +15,27 @@ $(document).ready(function(){
 	var rightp = false;
 	var leftp = false;
 
+	var brickrow = 3;
+	var brickcol = 5;
+	var brickw = 55;
+	var brickh = 10;
+	var brickpad = 2;
+	var brickt = 20;
+	var brickl = 8;
+	var bricks = [];
+	// init bricks Array
+	for(var c = 0; c < brickcol; c++){
+		bricks[c] = [];
+		for(var r = 0; r < brickrow; r++){
+			bricks[c][r] = {x: 0, y: 0, status: 1};
+		}
+	}
+
+	var score = 0;
+
+	var lives = 3;
+
+
 
 
 
@@ -26,16 +47,28 @@ $(document).ready(function(){
 		drawball();
 		// draw paddle
 		drawpaddle();
-		// update pos
+		// draw bricks
+		drawbricks();
+		// draw score
+		drawscore();
+		// draw lives
+		drawlives();
+		// collision check
 		colCheck();
+		brickcolcheck();
 		// key handling
 		eventCheck();
 	}
 	// event listeners
 	document.addEventListener("keydown", keydownh, false);
 	document.addEventListener("keyup", keyuph, false);
+	document.addEventListener("mousemove", mousecheck, false);
 	// gameloop interval
-	var interval = setInterval(draw, 10);
+	// var interval = setInterval(draw, 10);
+	draw();
+	// causes the draw function to call itself
+	// gives control of the framerate back to the browser
+	// requestAnimationFrame(draw);
 
 	// draw ball
 	function drawball(){
@@ -45,7 +78,7 @@ $(document).ready(function(){
 		ctx.fill();
 		ctx.closePath();
 	}
-	// collision check
+	// paddle collision check
 	function colCheck(){
 		if(x + dx > canvas.width - ballr || x + dx < ballr){
 			dx = -dx;
@@ -59,14 +92,30 @@ $(document).ready(function(){
 				dy = -dy;
 			}
 			else{
-				alert("GAME OVER");
-				document.location.reload();
-				// needed for chrome to end the game
-				clearInterval(interval);
+				gameover();
 			}
 		}
 		x += dx;
 		y += dy;
+	}
+
+	// brick collision check
+	function brickcolcheck(){
+		for(var c = 0; c < brickcol; c++){
+			for(var r = 0; r < brickrow; r++){
+				var b = bricks[c][r];
+				if(b.status == 1){
+					if(	x > b.x && x < b.x + brickw && y > b.y && y < b.y + brickh){
+						dy = -dy;
+						b.status = 0;
+						score++;
+						if(score == brickrow * brickcol){
+							win();
+						}
+					}
+				}
+			}
+		}
 	}
 
 	// draw paddle
@@ -76,6 +125,39 @@ $(document).ready(function(){
 		ctx.fillStyle = "#0095DD";
 		ctx.fill();
 		ctx.closePath();
+	}
+
+	// draw bricks
+	function drawbricks(){
+		for(var c = 0; c < brickcol; c++){
+			for(var r = 0; r < brickrow; r++){
+				if(bricks[c][r].status == 1){
+					var brickx = (c * (brickw + brickpad)) + brickl;
+					var bricky = (r * (brickh + brickpad)) + brickt;
+					bricks[c][r].x = brickx;
+					bricks[c][r].y = bricky;
+					ctx.beginPath();
+					ctx.rect(brickx, bricky, brickw, brickh);
+					ctx.fillStyle = "#0095DD";
+					ctx.fill();
+					ctx.closePath();
+				}				
+			}
+		}
+	}
+
+	// draw score
+	function drawscore(){
+		ctx.font = "12px Arial";
+		ctx.fillStyle = "#0095DD";
+		ctx.fillText("Score : " + score, 8, 15);
+	}
+
+	// draw lives
+	function drawlives(){
+		ctx.font = "12px Arial";
+		ctx.fillStyle = "#0095DD";
+		ctx.fillText("Lives: " + lives, canvas.width - 50, 15);
 	}
 
 	// event handling
@@ -95,6 +177,12 @@ $(document).ready(function(){
 			leftp = false;
 		}
 	}
+	function mousecheck(e){
+		var relx = e.clientX - canvas.offsetLeft;
+		if(relx > 0 && relx < canvas.width){
+			padx = relx - padw / 2;
+		}
+	}
 
 	// event check
 	function eventCheck(){
@@ -106,8 +194,28 @@ $(document).ready(function(){
 		}
 	}
 
+	// win
+	function win(){
+		alert("WINNER WINNER CHICKEN DINNER");
+		document.location.reload();
+		clearInterval(interval);
+	}
+
 	// game over
 	function gameover(){
-
+		lives--;
+		if(!lives){
+			alert("GAME OVER");
+			document.location.reload();
+			// needed for chrome to end the game
+			clearInterval(interval);
+		}
+		else{
+			x = canvas.width / 2;
+			y = canvas.height - 30;
+			dx = 2;
+			dy = -2;
+			padx = (canvas.width - padw) / 2;
+		}
 	}
 });
