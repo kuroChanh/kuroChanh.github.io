037c6585d02d15b74d2510cccf987118f7db0dd3
kuroChanh <tuyetnhi@hotmail.ca>
2019-04-10 19:12:44 -0700

    portfolio - added the image on the side and it works, need to add pictures and fix that scrolling issue

--- js/port.js
+++ js/port.js
@@ -1,119 +1,65 @@
 $(document).ready(function(){
-	//variables
-	var imgCount = 0;
-	//list of anime I'm currently watching
-	var currAnime = document.getElementById('aCurr');
-	var currTitle = document.getElementById('aTitle');
-	var currStatus = document.getElementById('aStatus');
-	var animeGallery = ['2b.png', 'akira2.png', 'akira.png', 'new.png', 'test.png'];
+	var currImage = document.getElementById('imgDisplay');
 
-	var next = false;
-	var prev = false;
-	var dotpress = false;
-
-	var dot0 = document.getElementById('dot0');
-	var dot1 = document.getElementById('dot1');
-	var dot2 = document.getElementById('dot2');
-	var dot3 = document.getElementById('dot3');
-	var dot4 = document.getElementById('dot4');
-
-	//variables
-	var container = document.getElementById('aSlider');
-	var preview = false;
-
-	function nextimg(){
-		if(next == true){
-			if(imgCount > animeGallery.length-1){
-				//goes to the beginning of the array
-				imgCount = 0;
-			}
-			//DIRECTORY FROM HTML
-			currAnime.setAttribute('style', 'background-image: url("images/' + animeGallery[imgCount] + '")');
-			next = false;
-		}
-		else if(prev == true){
-			if(imgCount < 0){
-				imgCount = animeGallery.length-1;
-			}
-			currAnime.setAttribute('style', 'background-image: url("images/' + animeGallery[imgCount] + '")');
-			prev = false;
-		}
-		else if(dotpress == true){
-			currAnime.setAttribute('style', 'background-image: url("images/' + animeGallery[imgCount] + '")');
-			dotpress = false;
-		}
-		if(imgCount == 0){
-			dot0.setAttribute('style', 'background-color: #717171');
-			dot1.setAttribute('style', 'background-color: #bbb');
-			dot2.setAttribute('style', 'background-color: #bbb');
-			dot3.setAttribute('style', 'background-color: #bbb');
-			dot4.setAttribute('style', 'background-color: #bbb');
-		}
-		else if(imgCount == 1){
-			dot1.setAttribute('style', 'background-color: #717171');
-			dot0.setAttribute('style', 'background-color: #bbb');
-			dot2.setAttribute('style', 'background-color: #bbb');
-			dot3.setAttribute('style', 'background-color: #bbb');
-			dot4.setAttribute('style', 'background-color: #bbb');
-			dot5.setAttribute('style', 'background-color: #bbb');
-		}
-		else if(imgCount == 2){
-			dot2.setAttribute('style', 'background-color: #717171');
-			dot1.setAttribute('style', 'background-color: #bbb');
-			dot0.setAttribute('style', 'background-color: #bbb');
-			dot3.setAttribute('style', 'background-color: #bbb');
-			dot4.setAttribute('style', 'background-color: #bbb');
-		}
-		else if(imgCount == 3){
-			dot3.setAttribute('style', 'background-color: #717171');
-			dot1.setAttribute('style', 'background-color: #bbb');
-			dot2.setAttribute('style', 'background-color: #bbb');
-			dot0.setAttribute('style', 'background-color: #bbb');
-			dot4.setAttribute('style', 'background-color: #bbb');
-		}
-		else if(imgCount == 4){
-			dot4.setAttribute('style', 'background-color: #717171');
-			dot1.setAttribute('style', 'background-color: #bbb');
-			dot2.setAttribute('style', 'background-color: #bbb');
-			dot3.setAttribute('style', 'background-color: #bbb');
-			dot0.setAttribute('style', 'background-color: #bbb');
-		}
-	};
-	$("#aLeft").click(function(){
-		prev = true;
-		imgCount--;
-		nextimg();
+	$("#p20").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
 	});
-	$("#aRight").click(function(){
-		next = true;
-		imgCount++;
-		nextimg();
+	$("#p19").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
 	});
-	nextimg();
-
-	$("#dot0").click(function(){
-		dotpress = true;
-		imgCount = 0;
-		nextimg();
+	$("#p18").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
 	});
-	$("#dot1").click(function(){
-		dotpress = true;
-		imgCount = 1;
-		nextimg();
+	$("#p17").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
 	});
-	$("#dot2").click(function(){
-		dotpress = true;
-		imgCount = 2;
-		nextimg();
+	$("#p16").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
 	});
-	$("#dot3").click(function(){
-		dotpress = true;
-		imgCount = 3;
-		nextimg();
+	$("#p15").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
 	});
-	$("#dot4").click(function(){
-		dotpress = true;
-		imgCount = 4;
-		nextimg();
+	$("#p14").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p13").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p12").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p11").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p10").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p9").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p8").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p7").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p6").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p5").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p4").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p3").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p2").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/akira2.png")');
+	});
+	$("#p1").click(function(){
+		currImage.setAttribute('style', 'background-image: url("images/window.png")');
+		console.log("Clicked");
 	});
 });
